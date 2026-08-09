b6b38906e72d891cb7f80f8b9f827d9d4f0c1db7
Chancelier Martin <196868237+ChancelierMartin@users.noreply.github.com>
2026-08-09 18:09:27 -0400

    style fix

--- SF_Engine/Engine/Version.hpp
+++ SF_Engine/Engine/Version.hpp
@@ -7,6 +7,6 @@
 namespace SF::Engine {
 constexpr uint16_t Engine_VERSION_MAJOR = 2026;
 constexpr uint8_t Engine_VERSION_MINOR = 8;
-constexpr uint16_t Engine_VERSION_PATCH = 174;
-constexpr std::string_view Engine_VERSION = "2026.08.174";
+constexpr uint16_t Engine_VERSION_PATCH = 178;
+constexpr std::string_view Engine_VERSION = "2026.08.178";
 } // namespace SF::Engine
