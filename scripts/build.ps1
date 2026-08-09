994b65ce87c861679c06c31b53a9da2495ce7674
Chancelier Martin <196868237+ChancelierMartin@users.noreply.github.com>
2026-08-09 14:45:44 -0400

    fahh bad idea

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -111,8 +111,5 @@ Import-VsDevEnvironment
 cmake --build --preset conan2-$buildType
 if ($LASTEXITCODE -ne 0) { Write-Host "Build failed" -ForegroundColor Red; Pop-Location; exit 1 }
 
-Remove-Item -Path "$out/Shaders" -Recurse -Force
-Copy-Item -Path "$PSScriptRoot/../SF_Engine/Shaders" -Destination "$out/" -Force
-
 Write-Host "==> Build succeeded" -ForegroundColor Green
 Pop-Location
