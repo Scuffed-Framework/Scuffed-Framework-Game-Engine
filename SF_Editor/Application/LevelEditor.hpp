97ef579a7cef829adc1b6cbaa20244ab5a74eb3f
ChancelierMartin <196868237+ChancelierMartin@users.noreply.github.com>
2026-08-19 22:21:24 -0700

    add command window to editor

--- SF_Editor/Application/LevelEditor.hpp
+++ SF_Editor/Application/LevelEditor.hpp
@@ -4,6 +4,7 @@
 #include "Panels/InspectorPanel.hpp"
 #include "Panels/BarPanels.hpp"
 #include "Panels/AssetsWindow.hpp"
+#include <Commands/CommandsWindow.hpp>
 
 namespace SF::Engine
 {
@@ -21,7 +22,8 @@ namespace SF::Engine
               hierarchy(std::make_unique<HierarchyPanel>()),
               inspector(std::make_unique<InspectorPanel>()),
               panels(std::make_unique<BarPanels>()),
-              assetBrowser(std::make_unique<AssetBrowser>())
+              assetBrowser(std::make_unique<AssetBrowser>()),
+              cmdWindow(std::make_unique<CommandWindow>())
         {
         }
 
@@ -51,5 +53,6 @@ namespace SF::Engine
         std::unique_ptr<InspectorPanel> inspector;
         std::unique_ptr<BarPanels> panels;
         std::unique_ptr<AssetBrowser> assetBrowser;
+        std::unique_ptr<CommandWindow> cmdWindow;
     };
 }
