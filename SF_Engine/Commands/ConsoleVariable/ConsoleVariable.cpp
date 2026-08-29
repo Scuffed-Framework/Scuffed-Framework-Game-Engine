38d1e020b7384792cc6a560ef6dcab0eb357d471
ChancelierMartin <196868237+ChancelierMartin@users.noreply.github.com>
2026-08-29 14:49:38 -0400

    Create ConsoleVariable.cpp

---
+++ SF_Engine/Commands/ConsoleVariable/ConsoleVariable.cpp
@@ -0,0 +1,11 @@
+#include "ConsoleVariable.hpp"
+
+namespace SF::Engine
+{
+    template<typename V>
+    ConsoleVariable<V>::ConsoleVariable(SFTL::string mod, SFTL::string nm, V defaultValue): module(std::move(mod)), name(std::move(nm)),
+              Value(defaultValue), LastValue(defaultValue)
+    {
+        ConsoleVariableRegistry::RegisterCVar(GetFullName(), this);
+    }
+}
