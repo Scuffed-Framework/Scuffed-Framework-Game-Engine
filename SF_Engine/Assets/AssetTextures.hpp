6cd651acf4381039551f1591f67dfba5ba71c16a
libmartinc <196868237+libmartinc@users.noreply.github.com>
2026-09-01 21:54:36 -0400

    i drank the t ig

--- SF_Engine/Assets/AssetTextures.hpp
+++ SF_Engine/Assets/AssetTextures.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <Rendering/Images/Image2d.hpp>
-#include <Assets/Bitmaps/PNG/BimapPNG.hpp>
+#include <Assets/Bitmaps/PNG/BitmapPNG.hpp>
 #include <cstring>
 
 namespace SF::Engine
