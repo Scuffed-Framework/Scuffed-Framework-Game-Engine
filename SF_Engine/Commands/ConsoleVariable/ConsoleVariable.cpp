a389874b7d190bc21aff7163bbefc1aa2d9562eb
libmartinc <196868237+libmartinc@users.noreply.github.com>
2026-09-03 20:27:09 -0400

    cvars

--- SF_Engine/Commands/ConsoleVariable/ConsoleVariable.cpp
+++ SF_Engine/Commands/ConsoleVariable/ConsoleVariable.cpp
@@ -3,8 +3,11 @@
 namespace SF::Engine
 {
     template<typename V>
-    ConsoleVariable<V>::ConsoleVariable(SFTL::string mod, SFTL::string nm, V defaultValue): module(std::move(mod)), name(std::move(nm)),
-              Value(defaultValue), LastValue(defaultValue)
+   ConsoleVariable<V>::ConsoleVariable(::SFTL::string mod, ::SFTL::string nm, V defaultValue)
+       : module(std::move(mod))
+       , name(std::move(nm))
+       , Value(defaultValue)
+       , LastValue(defaultValue)
     {
         ConsoleVariableRegistry::RegisterCVar(GetFullName(), this);
     }
--- SF_Engine/Commands/ConsoleVariable/ConsoleVariable.hpp
+++ SF_Engine/Commands/ConsoleVariable/ConsoleVariable.hpp
@@ -1,4 +1,10 @@
 #pragma once
+#include <charconv>
+#include <cstring>
+#include <string>
+#include <string_view>
+#include <type_traits>
+#include <unordered_map>
 
 #include <TemplateLibrary/Containers/String.hpp>
 #include <Engine/Module.hpp>
@@ -7,12 +13,26 @@
 
 namespace SF::Engine
 {
+    inline constexpr auto CommandWindowConsoleVariablePrefix = "CVar::";
+
     class ConsoleVariableRegistry;
+    struct IConsoleVariable : public Serializable
+    {
+    public:
+        ~IConsoleVariable() override = default;
+
+        [[nodiscard]] virtual ::SFTL::string GetFullName() const = 0;
+        [[nodiscard]] virtual bool DidValueChange() const = 0;
+
+        virtual bool SetValueFromString(::SFTL::string_view str) = 0;
+        [[nodiscard]] virtual ::SFTL::string ValueToString() const = 0;
+    };
 
     template<typename V>
-    struct ConsoleVariable : public Serializable
+    struct ConsoleVariable : public IConsoleVariable
     {
-        SF_RTTI_BASE(ConsoleVariable)
+        SF_RTTI(IConsoleVariable, ConsoleVariable)
+
     public:
         ::SFTL::string module;
         ::SFTL::string name;
@@ -20,12 +40,14 @@ namespace SF::Engine
         V LastValue;
 
         ConsoleVariable(::SFTL::string mod, ::SFTL::string nm, V defaultValue);
-
         ~ConsoleVariable() override = default;
 
-        [[nodiscard]] bool DidValueChange() const { return !(Value == LastValue); }
+        [[nodiscard]] bool DidValueChange() const override
+        {
+            return !(Value == LastValue);
+        }
 
-        [[nodiscard]] ::SFTL::string GetFullName() const
+        [[nodiscard]] ::SFTL::string GetFullName() const override
         {
             return module + "." + name;
         }
@@ -36,7 +58,61 @@ namespace SF::Engine
             Value = New;
         }
 
-        void Serialize(XMLNode &node) const override
+        bool SetValueFromString(::SFTL::string_view str) override
+        {
+            if constexpr (std::is_same_v<V, bool>)
+            {
+                if (str == "true" || str == "1")
+                {
+                    ChangeValue(true);
+                    return true;
+                }
+                if (str == "false" || str == "0")
+                {
+                    ChangeValue(false);
+                    return true;
+                }
+                return false;
+            }
+            else if constexpr (std::is_arithmetic_v<V>)
+            {
+                V parsed{};
+                auto [ptr, ec] = std::from_chars(str.Data(), str.Data() + str.Size(), parsed);
+                if (ec != std::errc{})
+                    return false;
+                ChangeValue(parsed);
+                return true;
+            }
+            else if constexpr (std::is_constructible_v<::SFTL::string, ::SFTL::string_view>)
+            {
+                // Assumes V is (or is constructible from) SFTL::string.
+                ChangeValue(::SFTL::string(str.Data()));
+                return true;
+            }
+            else
+            {
+                static_assert(!sizeof(V*), "SetValueFromString not implemented for this CVar type");
+                return false;
+            }
+        }
+
+        [[nodiscard]] ::SFTL::string ValueToString() const override
+        {
+            if constexpr (std::is_same_v<V, bool>)
+            {
+                return Value ? "true" : "false";
+            }
+            else if constexpr (std::is_arithmetic_v<V>)
+            {
+                return ::SFTL::string(std::to_string(Value));
+            }
+            else
+            {
+                return Value; // assumes SFTL::string-compatible
+            }
+        }
+
+        void Serialize(XMLNode& node) const override
         {
             XMLNode CVar = node.AddChild("CVar");
             CVar.SetAttribute("Module", module);
@@ -45,7 +121,7 @@ namespace SF::Engine
             CVar.SetAttribute("LastValue", LastValue);
         }
 
-        void Deserialize(const XMLNode &node) override
+        void Deserialize(const XMLNode& node) override
         {
             XMLNode CVar = node.GetChild("CVar");
             CVar.GetAttribute("Module", module);
@@ -60,18 +136,17 @@ namespace SF::Engine
         static inline bool reg = Register(ModuleStage::Always, Requires<>{});
         SF_RTTI(Module, ConsoleVariableRegistry)
 
-        // Non-owning: CVars are expected to outlive the registry (static/global lifetime).
-        inline static std::unordered_map<::SFTL::string, Serializable*> s_cvars;
+        inline static std::unordered_map<::SFTL::string, IConsoleVariable*> s_cvars;
 
     public:
-        static void RegisterCVar(const ::SFTL::string& fullName, Serializable* cvar)
+        static void RegisterCVar(const ::SFTL::string& fullName, IConsoleVariable* cvar)
         {
             s_cvars[fullName] = cvar;
         }
 
-        [[nodiscard]] static Serializable* Find(std::string_view fullName)
+        [[nodiscard]] static IConsoleVariable* Find(::SFTL::string_view fullName)
         {
-            auto it = s_cvars.find(::SFTL::string(&fullName));
+            auto it = s_cvars.find(::SFTL::string(fullName)); // :(
             return it != s_cvars.end() ? it->second : nullptr;
         }
 
@@ -79,16 +154,17 @@ namespace SF::Engine
         void Update() override {}
 
         [[nodiscard]] Stage GetStage() const override { return ModuleStage::Always; }
-        [[nodiscard]] std::string_view GetName() const override { return RTTI_GetTypeName(); }
+        [[nodiscard]] ::std::string_view GetName() const override { return RTTI_GetTypeName(); } // todo: add overload for sftl one
 
-        void Serialize(XMLNode &node) const
+        // NOLINTBEGIN(readability-convert-member-functions-to-static)
+        void Serialize(XMLNode& node) const
         {
             XMLNode registryNode = node.AddChild("ConsoleVariables");
             for (const auto& [fullName, cvar] : s_cvars)
                 cvar->Serialize(registryNode);
         }
 
-        void Deserialize(const XMLNode &node)
+        void Deserialize(const XMLNode& node)
         {
             XMLNode registryNode = node.GetChild("ConsoleVariables");
             for (XMLNode child = registryNode.GetFirstChild(); child.IsValid(); child = child.GetNextSibling())
@@ -96,10 +172,11 @@ namespace SF::Engine
                 ::SFTL::string mod, nm;
                 child.GetAttribute("Module", mod);
                 child.GetAttribute("Name", nm);
-                if (Serializable* cvar = Find(mod + "." + nm))
+                if (IConsoleVariable* cvar = Find(mod + "." + nm))
                     cvar->Deserialize(registryNode); // each cvar re-finds its own <CVar> child
             }
         }
+        // NOLINTEND(readability-convert-member-functions-to-static)
 
         Define_TypeId_Function(Module, ConsoleVariableRegistry)
     };
