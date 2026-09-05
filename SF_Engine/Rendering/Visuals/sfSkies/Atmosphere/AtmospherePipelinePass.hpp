800431253ce2ae0abd932593f7e80a40f1b987cb
libmartinc <196868237+libmartinc@users.noreply.github.com>
2026-09-04 21:14:04 -0400

    Update AtmospherePipelinePass.hpp

--- SF_Engine/Rendering/Visuals/sfSkies/Atmosphere/AtmospherePipelinePass.hpp
+++ SF_Engine/Rendering/Visuals/sfSkies/Atmosphere/AtmospherePipelinePass.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
+#include <Rendering/Buffers/UniformBuffer.hpp>
+#include <Rendering/Descriptors/DescriptorSet.hpp>
 #include <Rendering/PipelinePassManager.hpp>
 #include <Rendering/Pipelines/ComputePipeline.hpp>
-#include <Rendering/Descriptors/DescriptorSet.hpp>
-#include <Rendering/Buffers/UniformBuffer.hpp>
-#include "LUT/TransmittanceLUT.hpp"
 #include "LUT/MultiScatterLUT.hpp"
 #include "LUT/SkyViewLUT.hpp"
+#include "LUT/TransmittanceLUT.hpp"
 
 #include <Math/BasicMath.hpp>
 #include <memory>
@@ -22,28 +22,23 @@ namespace SF::Engine
     class AtmospherePipelinePass : public PipelinePass
     {
     public:
-        explicit AtmospherePipelinePass(Pipeline::Stage stage,
-                                        const AtmosphereParams &params = {});
+        explicit AtmospherePipelinePass(Pipeline::Stage stage, const AtmosphereParams &params = {});
         ~AtmospherePipelinePass() override = default;
 
-        void PreRender(const CommandBuffer &commandBuffer);
+        void PreRender(const CommandBuffer &commandBuffer) override;
 
         // Transitions "hdr" to VK_IMAGE_LAYOUT_GENERAL, dispatches the compute
         // shader, then transitions it back for whatever consumes it next.
         void Render(const CommandBuffer &commandBuffer) override;
 
         void SetSceneBuffers();
-        void SetFrameData(const Mat4 &invProj,
-                          const Mat4 &invView,
-                          const Vec3 &cameraPos,
-                          const Vec3 &planetPos,
-                          const Vec3 &sunDir,
-                          Vec2 screenSize);
+        void SetFrameData(const Mat4 &invProj, const Mat4 &invView, const Vec3 &cameraPos, const Vec3 &planetPos,
+                          const Vec3 &sunDir, Vec2 screenSize);
 
         void SetParams(const AtmosphereParams &params) { params_ = params; }
 
         AtmosphereParams &GetParams() { return params_; }
-        const AtmosphereParams &GetParams() const { return params_; }
+        [[nodiscard]] const AtmosphereParams &GetParams() const { return params_; }
 
     private:
         AtmosphereParams params_;
@@ -57,7 +52,7 @@ namespace SF::Engine
         // "hdr" is now both the compute target (binding 8, storage image) and
         // the thing we track for change detection, so lastColor_ doubles as
         // "which image we last wrote descriptor binding 8 for".
-        const Image2d *lastColor_ = nullptr;
+        const Image2d *lastColor_    = nullptr;
         const ImageDepth *lastDepth_ = nullptr;
     };
-}
+} // namespace SF::Engine
