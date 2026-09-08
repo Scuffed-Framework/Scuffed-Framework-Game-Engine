9674451cb5509ecd5e50345ae20b2ed14c1345c3
libmartinc <196868237+libmartinc@users.noreply.github.com>
2026-09-08 19:56:51 -0400

    Update String.hpp

--- SF_Engine/1stPartyLibs/TemplateLibrary/Containers/String.hpp
+++ SF_Engine/1stPartyLibs/TemplateLibrary/Containers/String.hpp
@@ -5,9 +5,19 @@ namespace SFTL
 {
     using string_view = AdvancedStringView<char>;
 
-    using string    = AdvancedString<char>;
-    using wstring   = AdvancedString<wchar_t>;
-    using u8string  = AdvancedString<char8_t>;
+    using string = AdvancedString<char>;
+    DEFINE_STRING_LITERAL_OPERATOR(string, _str)
+
+    using wstring = AdvancedString<wchar_t>;
+    DEFINE_STRING_LITERAL_OPERATOR(string, _wstr)
+
+    using u8string = AdvancedString<char8_t>;
+    DEFINE_STRING_LITERAL_OPERATOR(string, _u8str)
+
     using u16string = AdvancedString<char16_t>;
+    DEFINE_STRING_LITERAL_OPERATOR(string, _u16str)
+
     using u32string = AdvancedString<char32_t>;
+    DEFINE_STRING_LITERAL_OPERATOR(string, _u32str)
+    
 } // namespace SFTL
