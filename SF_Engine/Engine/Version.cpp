c235fa899bdf365b7283762670947af13967f869
libmartinc <196868237+libmartinc@users.noreply.github.com>
2026-09-08 20:01:39 -0400

    Update Version.cpp

--- SF_Engine/Engine/Version.cpp
+++ SF_Engine/Engine/Version.cpp
@@ -4,6 +4,6 @@
 namespace SF::Engine {
 const uint16_t Engine_VERSION_MAJOR = 2026;
 const uint8_t Engine_VERSION_MINOR = 9;
-const uint16_t Engine_VERSION_PATCH = 651;
-const std::string_view Engine_VERSION = "2026.09.651";
+const uint16_t Engine_VERSION_PATCH = 673;
+const std::string_view Engine_VERSION = "2026.09.673";
 } // namespace SF::Engine
