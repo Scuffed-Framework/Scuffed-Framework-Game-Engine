1c3359240684ab49250c0ec62f80e39764321966
libmartinc <196868237+libmartinc@users.noreply.github.com>
2026-09-09 18:18:32 -0400

    boom less weight

--- SF_Engine/1stPartyLibs/TemplateLibrary/UsefulMacros.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#define NO_DISCARD [[nodiscard]]
-#if defined(_MSC_VER) && !defined(__clang__)
-    #define FORCE_INLINE __forceinline
-#else
-    #define FORCE_INLINE inline __attribute__((__always_inline__))
-#endif
-#define UNUSED __attribute__((__unused__))
