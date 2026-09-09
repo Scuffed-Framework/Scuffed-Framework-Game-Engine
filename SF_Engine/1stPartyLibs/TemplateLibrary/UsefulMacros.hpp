4aacfc903382a37faa33ea6e9bed12f7b71edf90
libmartinc <196868237+libmartinc@users.noreply.github.com>
2026-09-08 20:01:30 -0400

    Update UsefulMacros.hpp

--- SF_Engine/1stPartyLibs/TemplateLibrary/UsefulMacros.hpp
+++ SF_Engine/1stPartyLibs/TemplateLibrary/UsefulMacros.hpp
@@ -1,2 +1,8 @@
 #pragma once
-#define NO_DISCARD [[__nodiscard__]]
+#define NO_DISCARD [[nodiscard]]
+#if defined(_MSC_VER) && !defined(__clang__)
+    #define FORCE_INLINE __forceinline
+#else
+    #define FORCE_INLINE inline __attribute__((__always_inline__))
+#endif
+#define UNUSED __attribute__((__unused__))
